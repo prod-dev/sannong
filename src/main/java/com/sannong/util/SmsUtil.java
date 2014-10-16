ce605d5e5cfbf78f03b1d456043f694a5f5ffe13
hnglng <hnglng@live.com>
2014-10-16 16:34:34 +0800

    Add spring security framework for project.

--- src/main/java/com/sannong/util/SmsUtil.java
+++ src/main/java/com/sannong/util/SmsUtil.java
@@ -1,4 +1,5 @@
 package com.sannong.util;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,62 +11,60 @@ import java.net.URLEncoder;
 import java.util.Random;
 
 public class SmsUtil {
-	private static Integer x_eid=11996;
-	private static String x_uid="techmio";
-	private static String x_pwd_md5="e10adc3949ba59abbe56e057f20f883e";
-	private static Integer x_gate_id=300;
+    private static Integer x_eid = 11996;
+    private static String x_uid = "techmio";
+    private static String x_pwd_md5 = "e10adc3949ba59abbe56e057f20f883e";
+    private static Integer x_gate_id = 300;
 
-	public static String sendSms(String mobile,String content) throws UnsupportedEncodingException{
-	Integer x_ac=10;//������Ϣ
-	HttpURLConnection httpconn = null;
-	String result="-20";
-	String memo = content.length()<70?content.trim():content.trim().substring(0, 70);
-	StringBuilder sb = new StringBuilder();
-	sb.append("http://gateway.woxp.cn:6630/utf8/web_api/?x_eid=");
-	sb.append(x_eid);
-	sb.append("&x_uid=").append(x_uid);
-	sb.append("&x_pwd_md5=").append(x_pwd_md5);
-	sb.append("&x_ac=").append(x_ac);
-	sb.append("&x_gate_id=").append(x_gate_id);
-	sb.append("&x_target_no=").append(mobile);
-	sb.append("&x_memo=").append(URLEncoder.encode(memo, "utf-8"));
-	try {
-	URL url = new URL(sb.toString());
-	httpconn = (HttpURLConnection) url.openConnection();
-	BufferedReader rd = new BufferedReader(new InputStreamReader(httpconn.getInputStream()));
-	result = rd.readLine();
-	rd.close();
-	} catch (MalformedURLException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-	} catch (IOException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-	} finally{
-	if(httpconn!=null){
-	httpconn.disconnect();
-	httpconn=null;
-	}
+    public static String sendSms(String mobile, String content) throws UnsupportedEncodingException {
+        Integer x_ac = 10;//������Ϣ
+        HttpURLConnection httpconn = null;
+        String result = "-20";
+        String memo = content.length() < 70 ? content.trim() : content.trim().substring(0, 70);
+        StringBuilder sb = new StringBuilder();
+        sb.append("http://gateway.woxp.cn:6630/utf8/web_api/?x_eid=");
+        sb.append(x_eid);
+        sb.append("&x_uid=").append(x_uid);
+        sb.append("&x_pwd_md5=").append(x_pwd_md5);
+        sb.append("&x_ac=").append(x_ac);
+        sb.append("&x_gate_id=").append(x_gate_id);
+        sb.append("&x_target_no=").append(mobile);
+        sb.append("&x_memo=").append(URLEncoder.encode(memo, "utf-8"));
+        try {
+            URL url = new URL(sb.toString());
+            httpconn = (HttpURLConnection) url.openConnection();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(httpconn.getInputStream()));
+            result = rd.readLine();
+            rd.close();
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (httpconn != null) {
+                httpconn.disconnect();
+                httpconn = null;
+            }
 
-	}
-	return result;
-	}
+        }
+        return result;
+    }
 
-    private static String    chars     = "0123456789";
-    private static Random    rnd         = new Random();	
-	// auto generate register code
-	public static String generateCode(int length)
-	{
-			if(length==0)return "";
-	        char[] buf = new char[length];	 
-	        for (int i = 0; i < buf.length; i++) {
-	            buf[i] = chars.charAt(rnd.nextInt(chars.length()));
-	        }	 
-	        return new String(buf);
-	 }
+    private static String chars = "0123456789";
+    private static Random rnd = new Random();
 
-		
-	
-	
+    // auto generate register code
+    public static String generateCode(int length) {
+        if (length == 0) return "";
+        char[] buf = new char[length];
+        for (int i = 0; i < buf.length; i++) {
+            buf[i] = chars.charAt(rnd.nextInt(chars.length()));
+        }
+        return new String(buf);
+    }
+
+
 }
 
--- src/test/java/com/sannong/util/SmsUtilTest.java
+++ src/test/java/com/sannong/util/SmsUtilTest.java
@@ -9,13 +9,17 @@ import org.junit.Test;
 
 public class SmsUtilTest {
 
+
 	@Test
 	public void testSms() throws UnsupportedEncodingException {
+        /*
 		String msg="welcome you register our website, your register code is "+SmsUtil.generateCode(6);
 		System.out.println(msg);
 		String ret=SmsUtil.sendSms("13128818478", msg);
 	    Integer result=new Integer(ret);
     	assertTrue("sms test success", result>0);
+    	*/
 	}
+
 
 }
