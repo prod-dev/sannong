76cc660a19c3ff818a7df21d82d4bab6dfe8538a
techmio <techmio@qq.com>
2014-10-17 17:18:43 +0800

    add email function with test case

---
+++ src/main/java/com/sannong/util/EmailUtil.java
@@ -0,0 +1,36 @@
+package com.sannong.util;
+import jodd.core.*;
+import jodd.mail.*;
+
+public class EmailUtil {
+	
+	public static boolean sendMail(String receiver,String subject,String content, boolean isHtml)
+	{
+		try
+		{
+			Email email = Email.create()
+			        .from("pactera99@163.com")
+			        .to(receiver)
+			        .subject(subject);
+			if(!isHtml)
+				email.addText(content);
+			else
+				email.addHtml(content);
+			AppSettings settings=new AppSettings();
+			SmtpServer smtpServer =
+			        new SmtpServer("smtp.163.com",
+			            new SimpleAuthenticator("pactera99", "pact_123"));
+			    SendMailSession session = smtpServer.createSession();
+			    session.open();
+			    session.sendMail(email);
+			    session.close();
+		}catch(Exception e)
+		{
+			return false;
+		}
+		
+		return true;
+		
+	}
+
+}
