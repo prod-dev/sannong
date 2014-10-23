252f6c397a0eff7cd2b540e0b0d154e64fd9b732
hnglng <hnglng@live.com>
2014-10-23 16:25:52 +0800

    Add cities table and records.

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -17,3 +17,14 @@ ADD PRIMARY KEY (`authorities_id`);
 
 ALTER TABLE `sannong`.`users` 
 ADD COLUMN `create_time` DATETIME NULL AFTER `update_time`;
+
+-- Add cities table by Bright Huang 2014-10-23
+DROP TABLE IF EXISTS `cities`;
+CREATE TABLE `cities` (
+  `city_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `city_index` bigint(20) NOT NULL,
+  `city_name` varchar(45) NOT NULL,
+  `city_code` varchar(45) NOT NULL,
+  `province_id`  bigint(20) NOT NULL,
+  PRIMARY KEY (`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
