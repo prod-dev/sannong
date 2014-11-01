8d766695437a4c1bc14f504d27d6f5f07f7a725c
hnglng <hnglng@live.com>
2014-11-01 22:09:52 +0800

    Add UI validation for myinfo.jsp page.

---
+++ src/main/webapp/content/static/js/sannong/myprofile.js
@@ -0,0 +1,266 @@
+/**
+ * Created by Bright Huang on 11/1/14.
+ */
+
+(function($) {
+
+    "use strict";
+
+    var myinfo = {};
+    myinfo.Model = {};
+    myinfo.View = {};
+
+    function validateForm(){
+        var validator = $("#myInfoForm").validate({
+            rules: {
+                "jobTitle": "required",
+                "company": "required",
+                "provinceSelect": "required",
+                "citySelect": "required",
+                "districtSelect": "required",
+                "companyAddress": "required",
+                "mailbox": {
+                    email:true
+                },
+                "deskPhone": {
+                    isTel: true
+                },
+                "cellphone": {
+                    required: true,
+                    isCellphone: true
+                },
+                "sms.smsValidationCode":{
+                    digits: true,
+                    rangelength:[4,4]
+                },
+                password: {
+                    required: true,
+                    minlength: 6
+                }
+            },
+            messages: {
+                "jobTitle": "必填",
+                "company": "必填",
+                "provinceSelect": "必填",
+                "citySelect": "必填",
+                "districtSelect": "必填",
+                "companyAddress": "必填",
+                "mailbox": "请输入正确格式的电子邮件",
+                "deskPhone": {
+                    isTel: "请输入正确格式的电话号码 如:010-12345678"
+                },
+                "cellphone": {
+                    required: "必填",
+                    isCellphone: "请正确填写您的手机号码",
+                    remote: "姓名或手机号码不存在"
+                },
+                "sms.smsValidationCode":{
+                    digits: "只能输入整数",
+                    rangelength: $.validator.format("请输入一个长度为{0}的整数验证码")
+                },
+                "password": {
+                    required: "请输入密码",
+                    minlength: $.validator.format("密码不能小于{0}个字 符")
+                }
+
+            },
+            success: function(label, element) {
+            },
+            errorPlacement:function(error,element) {
+                if ( element.is(":radio") ) {
+                    error.appendTo(element.parent().parent());
+
+                }else if ( element.is(":checkbox") ) {
+                    error.appendTo(element.next());
+
+                }else if (element.attr("name") == "cellphone") {
+                    error.insertAfter(element);
+                } else{
+                    error.insertAfter(element);
+                }
+
+            },
+            submitHandler:function(form){
+                form.submit();
+            }
+        });
+        return validator;
+    }
+
+    function addCities(){
+        var provinceIndex = $("#provinceSelect").val();
+        var options = {
+            url: 'getCities',
+            type: 'POST',
+            data: {'provinceIndex': provinceIndex},
+            success: function(data){
+                myinfo.Controller.addCitySelections(data);
+            },
+            fail: function(data){
+            }
+        }
+        myinfo.Controller.ajaxRequest(options);
+    }
+
+    function addProvinces(){
+        var options = {
+            url: 'getProvinces',
+            type: 'POST',
+            success: function(data){
+                myinfo.Controller.addProvinceSelections(data);
+            },
+            fail: function(error){
+            }
+        }
+        myinfo.Controller.ajaxRequest(options);
+    }
+
+    function addDistricts(){
+        var cityIndex= $('#citySelect').val();
+        var options = {
+            url: 'getDistricts',
+            type: 'POST',
+            data: {'cityIndex': cityIndex},
+            success: function(data){
+                myinfo.Controller.addDistrictSelections(data);
+            },
+            fail: function(data){
+
+            }
+        }
+        myinfo.Controller.ajaxRequest(options);
+    }
+
+    function init(){
+        $.validator.addMethod("isTel", function(value, element) {
+            var tel = /^\d{3,4}-?\d{7,9}$/; //电话号码格式010-12345678
+            return this.optional(element) || (tel.test(value));
+        }, "请正确填写您的电话号码");
+
+
+        $.validator.addMethod("isCellphone", function(value, element) {
+            var length = value.length;
+            var mobile = /^(((13[0-9]{1})|(15[0-9]{1})||(18[0-9]{1}))+\d{8})$/;
+            return this.optional(element) || (length == 11 && mobile.test(value));
+        }, "请正确填写您的手机号码");
+    }
+
+    myinfo.Controller = {
+        ajaxRequest : function(options) {
+            return $.ajax({
+                cache: false,
+                data: options.data,
+                dataType: options.dataType,
+                type: options.type,
+                url: options.url
+            }).success(function (data, status, xhr) {
+                options.success(data);
+            }).fail(function (xhr, status, error) {
+                options.fail(error);
+            }).always(function (xhr, status, error) {
+
+            });
+        },
+        addProvinceSelections: function(provinces) {
+            var provinceSelect = $('#provinceSelect');
+            $('#provinceSelect option').remove();
+            $('#citySelect option').remove();
+            $('#districtSelect option').remove();
+
+            for (var i in provinces){
+                var optionValue = provinces[i].provinceIndex;
+                var optionText = provinces[i].provinceName;
+                var option = "<option value=" + optionValue + ">" + optionText + "</option>";
+                provinceSelect.append(option);
+            }
+            addCities();
+        },
+        addCitySelections: function(cities) {
+            var citySelect = $('#citySelect');
+            $('#citySelect option').remove();
+            $('#districtSelect option').remove();
+
+            for (var i in cities){
+                var optionValue = cities[i].cityIndex;
+                var optionText = cities[i].cityName;
+                var option = "<option value=" + optionValue + ">" + optionText + "</option>";
+                citySelect.append(option);
+            }
+            addDistricts();
+        },
+
+        addDistrictSelections: function(districts) {
+            var districtSelect = $('#districtSelect');
+            $('#districtSelect option').remove();
+
+            for (var i in districts){
+                var optionValue = districts[i].districtIndex
+                var optionText = districts[i].districtName;
+                var option = "<option value=" + optionValue + ">" + optionText + "</option>";
+                districtSelect.append(option);
+            }
+        },
+        addEventListener: function(){
+            $("#provinceSelect").change(function(event){
+                addCities();
+            });
+
+            $("#citySelect").change(function(event){
+                $('#districtSelect option').remove();
+                addDistricts();
+            });
+
+            $("#register-btn").click(function(event){
+                if (validateForm().form() == true){
+                    $("#myInfoForm").submit();
+                }
+            });
+
+            $("#confirmedSubmit").click(function(event){
+                $("#myInfoForm").submit();
+            });
+
+
+            $("#validationCode").keyup(function(){
+                if (validateForm().element($("#validationCode")) == true && $("#validationCode").val() != ""){
+                    $("#applicationSubmit").removeAttr("disabled");
+                    $("#applicationSubmit").removeClass().addClass("btn btn-success");
+                } else {
+                    $("#applicationSubmit").attr({disabled: "disabled"});
+                    $("#applicationSubmit").removeClass().addClass("btn btn-default");
+                }
+
+            });
+
+            $("#action-send-code").click(function(event){
+                if (validateForm().form() == true){
+                    var options = {
+                        url: 'regcode',
+                        type: 'GET',
+                        data: {
+                            mobile: $("#cellphone").val(),
+                            smstype: $("#action-send-code").attr("data-type")
+                        },
+                        success: function(data){
+
+                        },
+                        fail: function(data){
+
+                        }
+                    }
+                    myinfo.Controller.ajaxRequest(options);
+                }
+            });
+        }
+    };
+
+    $(function() {
+        init();
+        addProvinces();
+        myinfo.Controller.addEventListener();
+
+    });
+
+    //Sannong.ProjectApplication = myinfo;
+    return myinfo;
+})(jQuery);
--- src/main/webapp/content/views/myinfo.jsp
+++ src/main/webapp/content/views/myinfo.jsp
@@ -1,18 +1,17 @@
 <%--
   Created by IntelliJ IDEA.
-  User: apple
+  User: Bright Huang
   Date: 10/14/14
   Time: 10:06
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 <html>
 <head>
     <link href="content/static/css/bootstrap-3.2.0/bootstrap.css" rel="stylesheet">
-     <link href="content/static/css/sannong/myinfo.css" rel="stylesheet">
-   <script src="http://code.jquery.com/jquery-2.1.1.js"></script>
-	<script src="content/static/js/sannong/myinfo.js?v=201410201404"> </script>
-	<script src="content/static/js/sannong/jquery.weebox.js?v=201410211946"></script>
+    <link href="content/static/css/sannong/myinfo.css" rel="stylesheet">
+    <link href="content/static/css/sannong/validation.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
@@ -26,14 +25,13 @@
             <jsp:include page='sidebar.jsp'/>
         </div>
         <div class="col-md-6 column">
-            <form role="form" id="applicationForm" action="modifyMyinfo" method="post">
+            <form role="form" id="myInfoForm" action="modifyMyinfo" method="post">
                 <div>
                     <div class="form-group">
-                        <label for="userName">姓名</label>
+                        <label for="realName">姓名</label>
+                        <input id="realName" class="form-control" name="realName" disabled value="${myinfo.realName}">
                     </div>
-                    <div class="form-group">
-                        <label id="realName" name="realName" >${myinfo.realName}</label>
-                    </div>
+
                     <div class="form-group">
                         <label for="jobTitle">职务</label>
                         <input type="input" class="form-control" id="jobTitle" name="jobTitle" placeholder="职务"  value="${myinfo.jobTitle}">
@@ -73,25 +71,30 @@
                         <input type="input" class="form-control" id="mailbox"  name="mailbox" placeholder="电子邮箱"  value="${myinfo.mailbox}">
                         <div class="errorDiv"></div>
                     </div>
-                    <div class="form-group" id="divCellphone">
-                        <label for="cellPhone">手机号码:</label>&nbsp;${myinfo.cellphone}&nbsp;&nbsp;&nbsp;&nbsp;<input type='button'   class="btn btn-sm btn-warning" onclick="script:$('#divCellphone').attr('style','display:none');$('#divEditCellphone').attr('style','display:block');"  value="更改手机号码">
+                    <div class="form-group form-inline" id="divCellphone">
+                        <label for="mobilphone">手机号码:</label>
+                        <input id="mobilphone" class="form-control" name="mobilphone" disabled value="${myinfo.cellphone}">
+                        <sec:authorize access="hasRole('ROLE_USER') and isAuthenticated()">
+                            <input type='button' class="btn btn-sm btn-warning" data-toggle="collapse" data-target="#demo" aria-expanded="false" aria-controls="#demo" value="更改手机号码">
+                        </sec:authorize>
+
                     </div>
-                    <div id="divEditCellphone" style="display:none">
-                    <div class="form-group form-inline" >
-                        <label for="cellPhone">手机号码</label>
-                        <div >
-                           <input type="hidden"  id="oldCellphone" value="${myinfo.cellphone}">
-                            <input type="input" class="input-short" id="cellphone" name="cellphone" placeholder="新手机号码">
-                            <input type="button" id="action-send-code" data-url="regcode" data-type="1" class="btn btn-sm btn-warning"  value="发送验证码">
-                            <input type="input" class="input-short" name="validationcode"  id="validationcode" placeholder="验证码">
-                            <div class="errorDiv"></div>
+                    <sec:authorize access="hasRole('ROLE_USER') and isAuthenticated()">
+                        <div id="demo" class="collapse">
+                            <div class="form-group form-inline">
+                                <div >
+                                    <input type="hidden"  id="oldCellphone" value="${myinfo.cellphone}">
+                                    <input type="text" class="form-control" id="cellphone" name="cellphone" placeholder="新手机号码" autocomplete="off">
+                                    <input type="button" id="action-send-code" data-url="regcode" data-type="1" class="btn btn-sm btn-warning" value="发送验证码">
+                                    <input type="text" class="form-control" name="validationcode"  id="validationcode" placeholder="验证码" autocomplete="off">
+                                    <div class="errorDiv"></div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                     
-                     </div>
-                    </div>
+                    </sec:authorize>
+                 </div>
                 <div class="errorDiv">${myinfomessage}</div>
-                <input type="button" id="register-btn"  class="btn btn-success" value="Submit">
+                <input type="button" id="register-btn"  name="myInfoSubmit" class="btn btn-success" value="提交">
             </form>
         </div>
         <div class="col-md-4 column"></div>
@@ -102,5 +105,7 @@
         <jsp:include page='footer.jsp'/>
     </div>
 </div>
+<script src="content/static/js/sannong/myprofile.js"> </script>
+
 </body>
 </html>
