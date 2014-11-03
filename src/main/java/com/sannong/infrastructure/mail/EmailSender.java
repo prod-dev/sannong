fc7576807e40982fa68e7495d10e497f0ed995e9
ben <ben@ben-M31EI-Series>
2014-11-03 14:49:42 +0800

    send mail with another thread

--- src/main/java/com/sannong/infrastructure/mail/EmailSender.java
+++ src/main/java/com/sannong/infrastructure/mail/EmailSender.java
@@ -1,36 +1,24 @@
 package com.sannong.infrastructure.mail;
+import org.apache.log4j.Logger;
+
 import com.sannong.infrastructure.util.MyConfig;
+import com.sannong.presentation.controller.LoginController;
 
 import jodd.mail.*;
 
 public class EmailSender {
-	
+
 	public static boolean sendMail(String receiver,String subject,String content, boolean isHtml)
 	{
-		try
-		{
-			Email email = Email.create()
-			        .from(MyConfig.getConfig("smtp-sender"))
-			        .to(receiver)
-			        .subject(subject);
-			if(!isHtml)
-				email.addText(content);
-			else
-				email.addHtml(content);
-			SmtpServer smtpServer =
-			        new SmtpServer(MyConfig.getConfig("smtp-server"),
-			            new SimpleAuthenticator(MyConfig.getConfig("smtp-account"), MyConfig.getConfig("smtp-password")));
-			    SendMailSession session = smtpServer.createSession();
-			    session.open();
-			    session.sendMail(email);
-			    session.close();
-		}catch(Exception e)
-		{
-			return false;
-		}
 		
-		return true;
+		
+		SendMailHander handler=new SendMailHander(receiver, subject, content, isHtml);
+		Thread myThread = new Thread(handler); 
+		myThread.start();
+		return true;		
 		
 	}
+	
+	
 
 }
