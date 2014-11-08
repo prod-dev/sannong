d287f9bcb07d12f467e415a8430cd3a7686c38b7
hnglng <hnglng@live.com>
2014-11-08 11:07:43 +0800

    Fix issue that user can't register in projectapplication.jsp after db table change.

--- src/main/java/com/sannong/service/impl/SmsServiceImpl.java
+++ src/main/java/com/sannong/service/impl/SmsServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -35,9 +36,13 @@ public class SmsServiceImpl implements ISmsService{
     public boolean updateSMS(HttpServletRequest request) {
         SMS sms = new SMS();
         String id = request.getParameter("smsid");
-        String time = request.getParameter("sendtime");
-        if (id.length() < 0) return false;
+        //String time = request.getParameter("sendtime");
+        Timestamp time = new Timestamp(System.currentTimeMillis());
+        if (id.length() < 0){
+            return false;
+        }
         long smsId = new Integer(id);
+        /*
         Date now = new Date();
         try {
             now = (new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")).parse(time);
@@ -45,6 +50,7 @@ public class SmsServiceImpl implements ISmsService{
             // TODO Auto-generated catch block
             time = (new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")).format(now);
         }
+        */
         sms.setSmsId(smsId);
         sms.setSendTime(time);
         sms.setSmsStatus(1);
@@ -65,63 +71,82 @@ public class SmsServiceImpl implements ISmsService{
             return 0;
         }
 
-    	if( request.getSession().getAttribute(SESSIOIN_SMS_CODES)==null) return 1;
-		Map<Date,String>	 map=(HashMap<Date,String>)request.getSession().getAttribute(SESSIOIN_SMS_CODES);
+    	if( request.getSession().getAttribute(SESSIOIN_SMS_CODES) == null){
+            return 1;
+        }
+
+		Map<Date,String> map = (HashMap<Date,String>)request.getSession().getAttribute(SESSIOIN_SMS_CODES);
 
 		Iterator iterator = map.entrySet().iterator();  
 		while (iterator.hasNext()) {  
 			Map.Entry mapEntry = (Map.Entry) iterator.next();  
-			Date dt=(Date)mapEntry.getKey();
-			String savecode=mapEntry.getValue().toString();
+			Date dt = (Date)mapEntry.getKey();
+			String savecode = mapEntry.getValue().toString();
 			if(savecode.equals(smsCode))
 			{
-				Date dtNow=new Date(System.currentTimeMillis()); 
-				Date dtSms=new Date(dt.getTime());
+				Date dtNow = new Date(System.currentTimeMillis());
+				Date dtSms = new Date(dt.getTime());
+
 				@SuppressWarnings("deprecation")
-				int diffInHours=dtNow.getHours()-dt.getHours();
-				@SuppressWarnings("deprecation")
-				int diffInMinuts=dtNow.getMinutes()-dt.getMinutes();
-				if(diffInHours==0&&diffInMinuts<5)
-					return 2;
-				else 
-					return 1;				
+				int diffInHours = dtNow.getHours() - dt.getHours();
+
+                @SuppressWarnings("deprecation")
+				int diffInMinuts = dtNow.getMinutes() - dt.getMinutes();
+				if(diffInHours == 0 && diffInMinuts < 5) {
+                    return 2;
+                }
+				else {
+                    return 1;
+                }
 			}			
 		}     	
-    	 return 0;
+    	return 0;
     }
 
     @SuppressWarnings("unchecked")
 	public boolean generateCode(HttpServletRequest request) {
-    	String mobile=request.getParameter("mobile").toString();
-    	String smstype=request.getParameter("smstype").toString();
-    	String regcode=SmsSender.generateCode(4);
-    	Map<Date,String>  map= new HashMap<Date,String>();
-    	if(mobile.length()<11)
+    	String mobile = request.getParameter("mobile").toString();
+    	String smstype = request.getParameter("smstype").toString();
+
+        String regcode = SmsSender.generateCode(4);
+
+    	Map<Date,String> map = new HashMap<Date,String>();
+    	if(mobile.length() < 11)
     		return false;
     	else    	
     	{
-    		SMS sms=new SMS();    		
-    		 sms.setCellphone(mobile);
-    		 sms.setSmsValidationCode(regcode); 
-    		 Date ts=new Date(System.currentTimeMillis()); 
-    		 if( request.getSession().getAttribute(MyConfig.SESSIOIN_SMS_CODES)!=null)
-    		 {
-    			 map=(HashMap<Date,String>)request.getSession().getAttribute(MyConfig.SESSIOIN_SMS_CODES);
-    		 }
-    		 map.put(ts,regcode);    			 
-    		 request.getSession().setAttribute(MyConfig.SESSIOIN_SMS_CODES, map);   
-    		 String content="验证码为:"+regcode;
-    		 if(smstype.equals("0"))
-    			 content=MyConfig.getConfig("sms-welcome").replace("{0}", regcode); 
-    		 if(smstype.equals("1"))
-    			 content=MyConfig.getConfig("sms-changeMobile").replace("{0}", regcode); 
-    			 //content="改变你在三农网上的注册手机号码，验证码为:"+regcode;   	
-    		 if(smstype.equals("2"))
-    			 content=MyConfig.getConfig("sms-newPassword").replace("{0}", regcode);
-    			//content="你在三农网上新密码为:"+regcode; 
-    		 sms.setSmsContent(content);
-    		 smsRepository.addNewSMS(sms);
-    		return true;
+            SMS sms = new SMS();
+
+            sms.setCellphone(mobile);
+            sms.setSmsValidationCode(regcode);
+
+            Date ts = new Date(System.currentTimeMillis());
+            if( request.getSession().getAttribute(MyConfig.SESSIOIN_SMS_CODES) != null)
+            {
+                map = (HashMap<Date,String>)request.getSession().getAttribute(MyConfig.SESSIOIN_SMS_CODES);
+            }
+            map.put(ts,regcode);
+
+            request.getSession().setAttribute(MyConfig.SESSIOIN_SMS_CODES, map);
+
+            String content="验证码为:" + regcode;
+            if(smstype.equals("0")) {
+                content = MyConfig.getConfig("sms-welcome").replace("{0}", regcode);
+            }
+            if(smstype.equals("1")) {
+                content = MyConfig.getConfig("sms-changeMobile").replace("{0}", regcode);
+                //content="改变你在三农网上的注册手机号码，验证码为:"+regcode;
+            }
+            if(smstype.equals("2")) {
+                content = MyConfig.getConfig("sms-newPassword").replace("{0}", regcode);
+                //content="你在三农网上新密码为:"+regcode;
+            }
+
+            sms.setSmsContent(content);
+            sms.setSendTime(new Timestamp(System.currentTimeMillis()));
+            smsRepository.addNewSMS(sms);
+
+            return true;
     	}
     }
 }
