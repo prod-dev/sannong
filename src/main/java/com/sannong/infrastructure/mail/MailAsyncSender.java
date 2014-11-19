461f27246243cdb1effe3674c8cb4bccbd52bdbc
hnglng <hnglng@live.com>
2014-11-19 12:07:39 +0800

    Fix the issue that mail subject is not supporting Chinese character.

--- src/main/java/com/sannong/infrastructure/mail/MailAsyncSender.java
+++ src/main/java/com/sannong/infrastructure/mail/MailAsyncSender.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 
+import javax.mail.internet.MimeUtility;
+
 /**
  * Created by Bright Huang on 11/17/14.
  */
@@ -35,9 +37,10 @@ public class MailAsyncSender {
             String smtpAccount = appConfig.getProperty("smtp-account");
             String smtpPassword = appConfig.getProperty("smtp-password");
             String mailOfReceivers = appConfig.getProperty("newApp-admin-email");
+            String mailSubject = appConfig.getProperty("newApp-email-subject");
 
             taskExecutor.execute(new SendTask(smtpSender, smtpServerName, smtpAccount,
-                    smtpPassword, mailOfReceivers, mailContent));
+                    smtpPassword, mailOfReceivers, mailContent, mailSubject));
 
         }catch(Exception ex){
             logger.error(ex.getMessage());
@@ -45,26 +48,26 @@ public class MailAsyncSender {
 
     }
 
-
     private class SendTask implements Runnable {
         private String smtpSender;
         private String smtpServerName;
         private String smtpAccount;
         private String smtpPassword;
         private String mailOfReceivers;
-        private String subject = "新的项目申请";
+        private String mailSubject;
         private boolean isHtml = false;
         private String mailContent;
         private SendMailSession session;
 
         public SendTask(String smtpSender, String smtpServer, String smtpAccount, String smtpPassword,
-                      String mailOfReceivers, String mailContent) {
+                      String mailOfReceivers, String mailContent, String mailSubject) throws Exception{
             this.smtpSender = smtpSender;
             this.smtpServerName = smtpServer;
             this.smtpAccount = smtpAccount;
             this.smtpPassword = smtpPassword;
             this.mailOfReceivers = mailOfReceivers;
             this.mailContent = mailContent;
+            this.mailSubject = mailSubject;
         }
 
         public void run() {
@@ -73,7 +76,7 @@ public class MailAsyncSender {
                 Email email = Email.create()
                         .from(smtpSender)
                         .to(emails)
-                        .subject(subject);
+                        .subject(MimeUtility.encodeText(mailSubject, "gb2312", "B"));
                 if (!isHtml) {
                     email.addText(mailContent);
                 }
