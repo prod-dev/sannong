69289a7a8e90545d69968db8f98143e6c720d81c
vidorchan <1282911204@qq.com>
2014-12-16 17:57:14 +0800

    fix update status bug in user-management.jsp

--- src/main/webapp/js/app/modules/user-management.js
+++ src/main/webapp/js/app/modules/user-management.js
@@ -75,11 +75,12 @@ define(['jquery', 'bootstrap', 'handlebars', 'sannong', 'validate', 'ajaxHandler
                         saveOrSubmit = answerStatusStr.substring(1, 2);
 
                     // about when admin can update user's questionnaire
-                    if (parseInt(questionnaireNo) > latestQuestionnaireNo ||
-                        ((parseInt(questionnaireNo) == latestQuestionnaireNo) && saveOrSubmit == 0)){
-                        $("#update").attr("disabled", "disabled");
+                    if(parseInt(questionnaireNo) > latestQuestionnaireNo){
+                        $("#update").removeClass("orange-bt-small").addClass("gray-bt-small");
+                        $("#update").attr("disabled",true);
                     }else{
-                        $("#update").attr("disabled", false);
+                        $("#update").removeClass("gray-bt-small").addClass("orange-bt-small");
+                        $("#update").attr("disabled",false);
                     }
 
                     if (data.applicant != null) {
